bbda1642f240ee33a36ac66e4378f08cda4a305e
shelble2 <31429032+shelble2@users.noreply.github.com>
2017-09-17 20:46:13 -0400

    add result array

--- module3/assignment_program.cu
+++ module3/assignment_program.cu
@@ -13,10 +13,11 @@
  
 char cpu_text[ARRAY_SIZE]; 
 char cpu_key[ARRAY_SIZE];
+char cpu_result[ARRAY_SIZE];
  
 __global__
  
-void encrypt(char *text, char *key)
+void encrypt(char *text, char *key, char *result)
 {
  		/* Calculate the current index */
  		const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
@@ -27,7 +28,7 @@ void encrypt(char *text, char *key)
 		//TODO: need to wrap around here in order to have printable ciphertext. Can just use decimal value though
  	
  		/* Save back in text array */
- 		text[thread_idx] = cipherchar; 
+ 		result[thread_idx] = cipherchar; 
 }
  
 void main_sub()
@@ -36,64 +37,69 @@ void main_sub()
  
  		printf("Encrypting text: \n");
  		for(i = 0; i < ARRAY_SIZE; i++)
-        {
+        	{
         		printf("%c", cpu_text[i]);
-        }
+        	}
                                       
  		printf("\n With Key: \n");    
  		for(i = 0; i < ARRAY_SIZE; i++) 
  		{
- 				printf("%c", cpu_key[i]);
-        }
+ 			printf("%c", cpu_key[i]);
+        	}
  
  		/* Declare and allocate pointers for GPU based parameters */
  		char *gpu_text;
  		char *gpu_key;
- 
+ 		char *gpu_result;
+	
  		cudaMalloc((void **)&gpu_text, ARRAY_SIZE_IN_BYTES);
  		cudaMalloc((void **)&gpu_key, ARRAY_SIZE_IN_BYTES);
+		cudaMalloc((void **)&gpu_result, ARRAY_SIZE_IN_BYTES);
  		
  		/* Copy the CPU memory to the GPU memory */
  		cudaMemcpy( cpu_text, gpu_text, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice);
  		cudaMemcpy( cpu_key, gpu_key, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice);
- 
+ 		cudaMemcpy( cpu_result, gpu_result, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice);
+	
  		/* Designate the number of blocks and threads */
  		const unsigned int num_blocks = ARRAY_SIZE/16;
  		const unsigned int num_threads = ARRAY_SIZE/num_blocks;
  
  		/* Execute the encryption kernel */
- 		encrypt<<<num_blocks, num_threads>>>(gpu_text, gpu_key);
+ 		encrypt<<<num_blocks, num_threads>>>(gpu_text, gpu_key, gpu_result);
  
  		/* Copy the GPU memory back to the CPU 
  		cudaMemcpy( cpu_text, gpu_text, ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost);
  		cudaMemcpy( cpu_key, gpu_key, ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost);*/
-
+		cudaMemcpy( cpu_result, gpu_result, ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost);
+	
  		/* Free the GPU memory */
  		cudaFree(gpu_text);
  		cudaFree(gpu_key);
+		cudaFree(gpu_result);
  
  		/* Print the final result */
-        printf("\nResults in ciphertext: \n");
-        for (i = 0; i < ARRAY_SIZE; i++) 
-        {
- 				printf("%d", cpu_text[i]);	
+        	printf("\nResults in ciphertext: \n");
+        	for (i = 0; i < ARRAY_SIZE; i++) 
+        	{
+ 			printf("%d", cpu_result[i]);	
  		}
  		printf("\n");                                     
  }
  
  int main()
  {
- 		/* TODO: get input file, key file, array size and num blocks from command line */
+ 	/* TODO: get input file, key file, array size and num blocks from command line */
  
         FILE *input_fp = fopen("input_text.txt", "r");
         FILE *key_fp = fopen("input_text.txt", "r");
         for(int i = 0; i < ARRAY_SIZE; i++) {
- 				cpu_text[i] = (char) fgetc(input_fp);
- 				cpu_key[i] = (char) fgetc(key_fp);  
- 		}
+ 		cpu_text[i] = (char) fgetc(input_fp);
+ 		cpu_key[i] = (char) fgetc(key_fp);  
+	}
  
- 		main_sub();
+ 	main_sub();
  	
- 		return EXIT_SUCCESS;
+ 	return EXIT_SUCCESS;
  }
  
