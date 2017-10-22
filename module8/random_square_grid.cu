814f472a5bb4076c8a870a9609cef54119f41ef0
shelble2 <shelble2@jhu.edu>
2017-10-22 19:52:13 -0400

    make multiple threads per block possible in kernels

--- module8/random_square_grid.cu
+++ module8/random_square_grid.cu
@@ -25,24 +25,28 @@
 
 /* this GPU kernel function is used to initialize the random states */
 __global__ void init(unsigned int seed, curandState_t* states) {
+  /* Calculate the current index */
+  const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
 
   /* we have to initialize the state */
   curand_init(seed, /* the seed can be the same for each core, here we pass the time in from the CPU */
-              blockIdx.x, /* the sequence number should be different for each core (unless you want all
-                             cores to get the same sequence of numbers for some reason - use thread id! */
+              idx, /* the sequence number should be different for each core (unless you want all
+                      cores to get the same sequence of numbers for some reason - use thread id! */
               0, /* the offset is how much extra we advance in the sequence for each call, can be 0 */
-              &states[blockIdx.x]);
+              &states[idx]);
 }
 
 /* this GPU kernel takes an array of states, and an array of ints, and puts a random int into each */
 __global__ void randoms(curandState_t* states, unsigned int* numbers) {
+  /* Calculate the current index */
+  const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+
   /* curand works like rand - except that it takes a state as a parameter */
-  numbers[blockIdx.x] = curand(&states[blockIdx.x]) % MAX_INT;
+  numbers[idx] = curand(&states[idx]) % MAX_INT;
 }
 
 int main( ) {
-  //TODO: Clean this up, make it print like a sudoku. Put it in array
-  // Can also do multiple threads per block
+  //TODO: Clean this up, make it print like a sudoku. Put it in 2D array
 
    /* CUDA's random number library uses curandState_t to keep track
       of the seed value
