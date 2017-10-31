37298811657f049715211da4ebc38006ea33c34e
shelble2 <shelble2@jhu.edu>
2017-10-30 23:20:42 -0400

    fixing up mirror.cpp and adding timing elements

--- module9/Assignment/mirror.cpp
+++ module9/Assignment/mirror.cpp
@@ -12,50 +12,60 @@
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
 #include <npp.h>
-#include <Exceptions.h>
 
+#include <Exceptions.h>
 #include <ImagesNPP.h>
 #include <ImagesCPU.h>
 #include <ImageIO.h>
 
-int main_sub(void)
+int mirror_sub(void)
 {
   std::string image_fn = "Lena.pgm";
- 
+
   // Construct the filename for the result
   std::string mirror_fn = image_fn;
   std::string::size_type dot = mirror_fn.rfind('.');
   if (dot != std::string::npos) {
     mirror_fn = mirror_fn.substr(0, dot);
   }
-
   mirror_fn += "_mirror.pgm";
 
+  cudaEvent_t start, stop;
+  float duration;
+
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+
+  /* Recording from load to copy back */
+  cudaEventRecord(start, 0);
+
+  // Load the original image into host memory
   npp::ImageCPU_8u_C1 h_original;
- try { 
-  npp::loadImage(image_fn, h_original);
- } catch (npp::Exception &rException) {
-  std::cerr << "Error! Exception occurred: " << std::endl;
-  std::cerr << rException << std::endl;
-  exit(EXIT_FAILURE);
+  try {
+    npp::loadImage(image_fn, h_original);
+  } catch (npp::Exception &rException) {
+    std::cerr << "Error! Exception occurred: " << std::endl;
+    std::cerr << rException << std::endl;
+    exit(EXIT_FAILURE);
  }
-  
+
   //Copy to device
   npp::ImageNPP_8u_C1 d_original(h_original);
 
-  // create struct for ROI size
   NppiSize size_ROI = {(int)d_original.width() , (int)d_original.height() };
 
   //Declare a pointer for the result
   npp::ImageNPP_8u_C1 d_mirror(d_original.size());
- try { 
-  NppStatus status = nppiMirror_8u_C1R(d_original.data(), d_original.pitch(), d_mirror.data(), d_mirror.pitch(), size_ROI, NPP_HORIZONTAL_AXIS);
-  printf("Result of nppiMirror is %d\n", status);
- } catch (npp::Exception &rException) {
-  std::cerr << "Error! Exception occurred: " << std::endl;
-  std::cerr << rException << std::endl;
-  exit(EXIT_FAILURE);
- }
+
+  // Make a mirror image of the original
+  try {
+    NppStatus status = nppiMirror_8u_C1R(d_original.data(), d_original.pitch(), d_mirror.data(), d_mirror.pitch(), size_ROI, NPP_HORIZONTAL_AXIS);
+    printf("Result of nppiMirror is %d\n", status);
+  } catch (npp::Exception &rException) {
+    std::cerr << "Error! Exception occurred: " << std::endl;
+    std::cerr << rException << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
   // Make host destination
   npp::ImageCPU_8u_C1 h_mirror(h_original.size());
@@ -63,10 +73,16 @@ int main_sub(void)
   // Copy it back
   d_mirror.copyTo(h_mirror.data(), h_mirror.pitch());
 
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&duration, start, stop);
+  printf("\nElapsed Time: %f\n", duration);
+
   // Save it to file
   saveImage(mirror_fn, h_mirror);
   std::cout << "Saved image to file: " << mirror_fn << std::endl;
 
+  // Free Everything
   nppiFree(d_original.data());
   nppiFree(d_mirror.data());
   nppiFree(h_original.data());
@@ -75,7 +91,13 @@ int main_sub(void)
   exit(EXIT_SUCCESS);
 }
 
-int main(void) 
+int main(void)
 {
- main_sub();
+  printf("\nRun 1 of mirror program\n");
+  mirror_sub();
+
+  printf("\nRun 2 of mirror program\n");
+  mirror_sub();
+
+  printf("\n");
 }
